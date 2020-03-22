bf9148588fe9b7a831afa96c357a6720315ee5c4
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-03-22 23:31:47 +0200

    Persons can move

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ enum State {
   Dead = "Dead"
 }
 
-class Position {
+class Vector {
   x: number
   y: number
   constructor(x: number, y: number) {
@@ -17,31 +17,80 @@ class Position {
 }
 
 class Person {
-  position: Position
+  worldDimensions: WorldDimensions
+  position: Vector
+  speed: Vector
   interactionRange: number = 2
   state: State
-  constructor(position: Position, state: State) {
+  constructor(worldDimensions: WorldDimensions, position: Vector, speed: Vector, state: State) {
+   this.worldDimensions = worldDimensions;
    this.position = position;
+   this.speed = speed;
    this.state = state;
+  }
+
+  move() {
+    if (this.position.x >= this.worldDimensions.width || this.position.x <= 0) {
+      this.speed.x = - this.speed.x;
+    }
+    if (this.position.y >= this.worldDimensions.height || this.position.y <= 0) {
+      this.speed.y = - this.speed.y;
+    }
+    this.position.x = this.position.x + this.speed.x * timeStep;
+    this.position.y = this.position.y + this.speed.y * timeStep;
   }
 }
 
-class World {
-  width: number
+const timeStep = 1;
+const maxSpeed = 5;
+
+function randomUpTo(value: number): number {
+  return Math.round(Math.random() * value);
+}
+
+function randomSign(): number {
+  return Math.random() > 0.5 ? 1 : -1;
+}
+
+function randomOfMagnitude(magnitude: number): number {
+  return randomSign() * randomUpTo(magnitude);
+}
+
+interface WorldDimensions {
+  width: number,
   height: number
+}
+
+class World {
+  dimensions: WorldDimensions
   population: Array<Person>
-  constructor(options: { width: number, height: number }) {
-    this.width = options.width;
-    this.height = options.height;
+  constructor(dimensions: WorldDimensions) {
+    this.dimensions = dimensions;
   }
 
   populate(populationSize: number) {
     this.population = [];
     for (let i = 0; i < populationSize; i++) {
-      const position = new Position(Math.round(Math.random() * this.width), Math.round(Math.random() * this.height))
-      const person = new Person(position, State.Healthy)
+      const position = new Vector(randomUpTo(this.dimensions.width), randomUpTo(this.dimensions.height))
+      const speed = new Vector(randomOfMagnitude(maxSpeed), randomOfMagnitude(maxSpeed))
+      const person = new Person(this.dimensions, position, speed, State.Healthy)
       this.population.push(person);
     }
+  }
+
+  show(context: CanvasRenderingContext2D) {
+    context.clearRect(0, 0, this.dimensions.width, this.dimensions.height);
+    this.population.forEach(person => {
+      context.beginPath();
+      context.arc(person.position.x, person.position.y, person.interactionRange, 0, 2 * Math.PI);
+      context.stroke();
+    });
+  }
+
+  update() {
+    this.population.forEach(person => {
+      person.move();
+    });
   }
 }
 
@@ -52,13 +101,12 @@ console.log(world.population[12])
 
 window.addEventListener('load', () => {
   const canvas = <HTMLCanvasElement> document.getElementById('worldVisualization');
-  canvas.width = world.width;
-  canvas.height = world.height;
+  canvas.width = world.dimensions.width;
+  canvas.height = world.dimensions.height;
   const context = canvas.getContext('2d');
 
-  for (let person of world.population) {
-    context.beginPath();
-    context.arc(person.position.x, person.position.y, person.interactionRange, 0, 2 * Math.PI);
-    context.stroke();
-  }
+  setInterval(() => {
+    world.show(context);
+    world.update();
+  }, 100);
 });
