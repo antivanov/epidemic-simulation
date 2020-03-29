313d28cd90e5b390aa49da667cd03f6f0d5f39e1
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-03-29 14:51:39 +0300

    Display infected persons

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,10 +1,21 @@
-import { World, Person, worldDimensions } from '../common/common';
+import { World, Person, State, worldDimensions } from '../common/common';
+
+//TODO: type?
+const fillStyles = {
+  [State.Healthy]: "green",
+  [State.Infected]: "orange",
+  [State.Contagious]: "#ff7b25",
+  [State.Accute]: "#c94c4c",
+  [State.Immune]: "#034f84",
+  [State.Dead]: "#b2b2b2"
+};
 
 //TODO: World should have the correct type
 function show(world: World, context: CanvasRenderingContext2D) {
   //TODO: Read from a common configuration
   context.clearRect(0, 0, worldDimensions.width, worldDimensions.height);
   world.population.forEach((person: Person) => {
+    context.strokeStyle = fillStyles[person.state];
     context.beginPath();
     context.arc(person.position.x, person.position.y, person.interactionRange, 0, 2 * Math.PI);
     context.stroke();
