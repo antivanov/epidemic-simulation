faf15e26f189ee962e664f42d700e632121b741f
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-04-13 15:57:05 +0300

    Generate Vue scaffolding and customize it

---
+++ src/ui-vue/src/components/World.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <span>World visualization</span>
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class World extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="stylus">
+div
+  margin 40px 0 0
+</style>
---
+++ src/ui-vue/src/views/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-page">
+    <World msg="Under construction"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import World from '@/components/World.vue'
+
+export default {
+  name: 'World',
+  components: {
+    World
+  }
+}
+</script>
