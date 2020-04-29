e3da643bf9bdf2abe1f3a89507321fc8b3e0778d
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-04-29 22:20:24 +0300

    Extract a separate SimulationsActions component

---
+++ src/ui/src/components/SimulationActions.vue
@@ -0,0 +1,28 @@
+<template>
+  <!--TODO: Buttons should be enabled/disabled based on the simulation state -->
+  <v-card-actions>
+    <v-spacer />
+    <!-- TODO: Update the button label/behavior based on the current state of the simulation as required -->
+    <v-btn color="primary" v-on:click="start">Start/Pause</v-btn>
+    <v-btn v-on:click="stop">Stop</v-btn>
+  </v-card-actions>
+</template>
+
+<script lang="ts">
+import Api from "../api";
+
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class SimulationActions extends Vue {
+  start() {
+    Api.start();
+  }
+
+  stop() {
+    Api.stop();
+  }
+}
+</script>
+
+<style scoped lang="stylus"></style>
--- src/ui/src/views/MainPage.vue
+++ src/ui/src/views/MainPage.vue
@@ -7,14 +7,7 @@
         >
           <v-col cols="10">
             <v-card>
-              <!-- TODO: Extract actions as a separate component -->
-              <!--TODO: Buttons should be enabled/disabled based on the simulation state -->
-              <v-card-actions>
-                <v-spacer />
-                <!-- TODO: Update the button label/behavior based on the current state of the simulation as required -->
-                <v-btn color="primary" v-on:click="start">Start/Pause</v-btn>
-                <v-btn v-on:click="stop">Stop</v-btn>
-              </v-card-actions>
+              <SimulationsActions />
               <v-container fluid>
                 <v-row>
                   <v-col align="center" justify="center">
@@ -42,23 +35,17 @@
 <script>
 
   // @ is an alias to /src
-  import PopulationView from '@/components/PopulationView.vue';
-  import StatisticsView from '@/components/StatisticsView.vue';
-  import Api from '../api';
+  import PopulationView from '@/components/PopulationView.vue'
+  import StatisticsView from '@/components/StatisticsView.vue'
+  import SimulationsActions from '@/components/SimulationActions.vue'
+  import Api from '../api'
 
   export default {
     name: 'MainPage',
-    methods: {
-      start() {
-        Api.start();
-      },
-      stop() {
-        Api.stop();
-      }
-    },
     components: {
       PopulationView,
-      StatisticsView
+      StatisticsView,
+      SimulationsActions
     }
   }
 </script>
