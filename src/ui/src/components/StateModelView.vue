177dd6300bf0eab8b59c672b8b3a1c81bce31b5f
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-05-07 00:01:28 +0300

    Draw some states with labels

--- src/ui/src/components/StateModelView.vue
+++ src/ui/src/components/StateModelView.vue
@@ -7,6 +7,13 @@ import * as d3 from 'd3';
 
 import { Component, Prop, Vue } from "vue-property-decorator";
 
+const stateNodeRadius = 15;
+const stateNodes = [
+  {x: 30, y: 50, label: 'Healthy'},
+  {x: 150, y: 50, label: 'Exposed'},
+  {x: 60, y: 120, label: 'Immune'}
+];
+
 @Component
 export default class StateModelView extends Vue {
 
@@ -14,14 +21,22 @@ export default class StateModelView extends Vue {
     //TODO: Better integration with Vue or do we really need to use D3? Can we just use Vue templates and components instead?
     const svg = d3.select('#stateModelView');
 
-    const circle = svg.selectAll('circle')
-       .data([32, 57, 112, 293]);
+    const group = svg.selectAll('g')
+      .data(stateNodes)
+      .enter()
+      .append('g')
+      .attr('transform', d => `translate(${d.x}, ${d.y})`);
 
-    const circleEnter = circle.enter().append('circle');
+    group.append('circle')
+      .attr('class', 'nodes')
+      .attr('fill', 'none')
+      .attr('stroke', 'black')
+      .attr('r', stateNodeRadius);
 
-    circleEnter.attr('cy', 60);
-    circleEnter.attr('cx', (d, i) => i * 100 + 30);
-    circleEnter.attr('r', d => Math.sqrt(d));
+    group.append('text')
+      .attr('dx', d => stateNodeRadius + 5)
+      .attr('dy', d => 5)
+      .text(d => d.label);
   }
 }
 </script>
