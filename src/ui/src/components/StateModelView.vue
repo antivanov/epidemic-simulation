426a101c805e5b8c8aaacc13b31c6278f801fc2c
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-05-12 23:28:19 +0300

    Initial code to draw the state transitions

--- src/ui/src/components/StateModelView.vue
+++ src/ui/src/components/StateModelView.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h3>State model</h3>
-    <svg id="stateModelView"></svg>
+    <svg id="stateModelView">
+      <defs>
+        <marker id="arrow" markerWidth="10" markerHeight="10" refX="10" refY="3" orient="auto" markerUnits="strokeWidth">
+          <path d="M0,0 L0,6 L9,3 z" fill="gray" />
+        </marker>
+      </defs>
+    </svg>
   </div>
 </template>
 
@@ -24,13 +30,62 @@ const stateNodes = [
   {x: 480, y: 380, state: State.Dead},
 ];
 
-const transitions = [{
+//TODO: Proper type?
+const stateNodePositions = stateNodes.reduce((acc: {[key: string]: {x: number, y: number}}, currentStateNode) => {
+  acc[currentStateNode.state.toString()] = {x: currentStateNode.x, y: currentStateNode.y};
+  return acc;
+}, {});
 
-}];
+const stateTransitions = [
+  {
+    from: State.Exposed,
+    to: State.Infected
+  },
+  {
+    from: State.Exposed,
+    to: State.Healthy
+  },
+  {
+    from: State.Infected,
+    to: State.Contagious
+  },
+  {
+    from: State.Contagious,
+    to: State.Accute
+  },
+  {
+    from: State.Accute,
+    to: State.Immune
+  },
+  {
+    from: State.Accute,
+    to: State.IntensiveCare
+  },
+  {
+    from: State.Contagious,
+    to: State.Immune
+  },
+  {
+    from: State.IntensiveCare,
+    to: State.Immune
+  },
+  {
+    from: State.IntensiveCare,
+    to: State.Dead
+  },
+  {
+    from: State.Immune,
+    to: State.Healthy
+  }
+];
 
 //TODO: Draw transitions between nodes
 
-//TODO: Fill the node with the color of the corresponding state (extract common state colors to somewhere inside the ui part?)
+//TODO: Draw separate special transition from healthy to exposed (happens due to an outside interference)
+
+//TODO: Fill the node with the color of the corresponding state (extract common state colors to somewhere inside the ui part?) - OK
+
+
 //TODO: Visualize the transition parameters: probability, base duration
 //TODO: Allow to change the transition parameters, listen for the changes and log updates parameters
 //TODO: Draw the whole state model
@@ -47,22 +102,68 @@ export default class StateModelView extends Vue {
   mounted() {
     const svg = d3.select('#stateModelView');
 
-    const group = svg.selectAll('g')
+    const nodeGroup = svg.selectAll('g')
       .data(stateNodes)
       .enter()
       .append('g')
       .attr('transform', d => `translate(${d.x}, ${d.y})`);
 
-    group.append('circle')
+    nodeGroup.append('circle')
       .attr('class', 'nodes')
       .attr('fill', d => stateColors[d.state])
       .attr('stroke', 'black')
       .attr('r', stateNodeRadius);
 
-    group.append('text')
+    nodeGroup.append('text')
       .attr('dx', d => stateNodeRadius + 5)
       .attr('dy', d => 5)
       .text(d => d.state);
+
+    //<line x1="295" y1="50" x2="95" y2="75" stroke="#000" stroke-width="5" marker-end="url(#arrow)" />
+    const arrow = svg.selectAll('line')
+      .data(stateTransitions.map(({from, to}) => {
+        const fromPosition = stateNodePositions[from];
+        const toPosition = stateNodePositions[to];
+        let x1 = fromPosition.x;
+        let y1 = fromPosition.y;
+        let x2 = toPosition.x;
+        let y2 = toPosition.y;
+
+        const angle = Math.atan((y2 - y1)/(x2 - x1));
+
+        //FIXME: Fix the computation so that all the arrows are displayed with the correct margin
+        const xMargin = Math.cos(angle) * stateNodeRadius;
+        const yMargin = Math.sin(angle) * stateNodeRadius;
+
+        if (x1 < x2) {
+          x1 = x1 + xMargin;
+          x2 = x2 - xMargin;
+        } else {
+          x1 = x1 - xMargin;
+          x2 = x2 + xMargin;
+        }
+
+        if (y1 < y2) {
+          y1 = y1 + yMargin;
+          y2 = y2 - yMargin;
+        } else {
+          y1 = y1 - yMargin;
+          y2 = y2 + yMargin;
+        }
+
+        return {
+          x1, x2, y1, y2
+        };
+      }))
+      .enter()
+      .append('line')
+      .attr('x1', d => d.x1)
+      .attr('y1', d => d.y1)
+      .attr('x2', d => d.x2)
+      .attr('y2', d => d.y2)
+      .attr('stroke', 'gray')
+      .attr('stroke-width', '2')
+      .attr('marker-end', 'url(#arrow)');
   }
 }
 </script>
